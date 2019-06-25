43130c0691283bbced631b427d7a366af1285722
Alex Oh <alexhboh@gmail.com>
2019-06-25 17:48:28 -0400

    Added the if command for concert search

--- liri.js
+++ liri.js
@@ -5,12 +5,15 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 
-
-var name = process.argv[2];
+var command = process.argv[2];
+var name = process.argv[3];
 const axios = require('axios');
-axios.get("https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp")
-  .then(function(response){
-    console.log(response.data[0].venue.name); // ex.: { user: 'Your User'}
-    console.log(response.data[0].venue.city + ", " + response.data[0].venue.region + ". " + response.data[0].venue.country);
-    console.log(moment(response.data[0].datetime).format('LL'));
-  });  
+
+if (command == "concert-this") {
+    axios.get("https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            console.log(response.data[0].venue.name); // ex.: { user: 'Your User'}
+            console.log(response.data[0].venue.city + ", " + response.data[0].venue.region + ". " + response.data[0].venue.country);
+            console.log(moment(response.data[0].datetime).format('LL'));
+        });
+}
